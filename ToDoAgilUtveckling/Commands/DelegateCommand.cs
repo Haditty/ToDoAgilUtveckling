b06f6346914a340ee29ca3e2ee2c55227e57412b
HADI-ITHÖGSKOLA\hadid <hadi.daliri@iths.se>
2026-05-20 19:31:36 +0200

    Delegate commands added for adding and removing items

---
+++ ToDoAgilUtveckling/Commands/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoAgilUtveckling.Commands
+{
+    public class DelegateCommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public DelegateCommand(Action<object> execute, Func<object?, bool>? canExecute = null)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute is null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
--- ToDoAgilUtveckling/ViewModels/MainViewModel.cs
+++ ToDoAgilUtveckling/ViewModels/MainViewModel.cs
@@ -5,18 +5,20 @@ using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using ToDoAgilUtveckling.Commands;
 using ToDoAgilUtveckling.Models;
 
 namespace ToDoAgilUtveckling.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        private ToDoItem _selectedItem;
+        public DelegateCommand AddItemCommand { get; }
+        public DelegateCommand DeleteItemCommand { get; }
 
+        private ToDoItem _selectedItem;
         public ToDoItem SelectedItem
         {
             get => _selectedItem;
-
             set
             {
                 _selectedItem = value;
@@ -25,36 +27,34 @@ namespace ToDoAgilUtveckling.ViewModels
         }
 
         private int _selectedIndex;
-
         public int SelectedIndex
         {
             get => _selectedIndex;
-
             set
             {
                 _selectedIndex = value;
                 RaisePropertyChanged();
-                if (_selectedIndex >= 0 && _selectedIndex < ToDoItems.Count) { SelectedItem = ToDoItems[_selectedIndex]; }
-                SelectedItem = ToDoItems[SelectedIndex];
+                if (_selectedIndex >= 0 && _selectedIndex < ToDoItems.Count)
+                {
+                    SelectedItem = ToDoItems[_selectedIndex];
+                }
             }
         }
-        
 
         private ObservableCollection<ToDoItem> _toDoItems;
-
         public ObservableCollection<ToDoItem> ToDoItems {
             get => _toDoItems;
             set
             {
                 _toDoItems = value;
                 RaisePropertyChanged();
-           
             }
         }
 
-
         public MainViewModel()
         {
+            AddItemCommand = new DelegateCommand(AddTask, CanAddTask);
+            DeleteItemCommand = new DelegateCommand(RemoveTask, CanRemoveTask);
             using (var db = new AppDbContext())
             {
                      
@@ -62,7 +62,33 @@ namespace ToDoAgilUtveckling.ViewModels
                     db.ToDoItems.ToList()        
                 );
             }
-          
+            SelectedIndex = 0;
+        }
+
+        private void AddTask(object? obj)
+        {
+            ToDoItems.Add(new ToDoItem()
+            {
+                Id = ToDoItems.Count +1,
+                Title = "",
+                Description = "",
+                CategoryId = 1,
+                IsDone = false
+            });
+        }
+
+        private void RemoveTask(object? obj)
+        {
+            ToDoItems.Remove(ToDoItems[SelectedIndex]);
+        }
+        private bool CanAddTask (object? obj)
+        {
+            return true;
+        }
+
+        private bool CanRemoveTask(object? obj)
+        {
+            return true;
         }
     }
 }
