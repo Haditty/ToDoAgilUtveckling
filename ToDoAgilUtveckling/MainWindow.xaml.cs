923c634527388ae0c2f26dd22c961a1b1798142f
Mojtaba Akbari <mojtaba.akbari@iths.se>
2026-05-20 18:08:50 +0200

    som small conflicts

--- ToDoAgilUtveckling/MainWindow.xaml.cs
+++ ToDoAgilUtveckling/MainWindow.xaml.cs
@@ -12,9 +12,7 @@ using ToDoAgilUtveckling.ViewModels;
 
 namespace ToDoAgilUtveckling
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
